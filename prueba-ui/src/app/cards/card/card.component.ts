47c5167ec1ae19fa0a78f82fdedb80e92532a0cd
Carlos Castillo Martinez <ccmartinez@uoc.edu>
2024-05-18 10:17:10 +0200

    Refactor card component code

--- prueba-ui/src/app/cards/card/card.component.ts
+++ prueba-ui/src/app/cards/card/card.component.ts
@@ -10,16 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-
 export class CardComponent {
-  @Input() city!: City;
+  @Input() city!: City; // Input to receive city data
 
-  @ViewChild('expandBtn') expandBtn!: ElementRef;
-  @ViewChild('cityDescription') cityDescription!: ElementRef;
+  @ViewChild('expandBtn') expandBtn!: ElementRef; // Reference to the expand button element
+  @ViewChild('cityDescription') cityDescription!: ElementRef; // Reference to the city description element
 
-  //Toggle expanded text
+  // Method to toggle the expanded state of the city description
   toggleText() {
+    // Toggle 'rotated' class on the expand button
     this.expandBtn.nativeElement.classList.toggle('rotated');
+
+    // Toggle 'cutoff-text' and 'expanded' classes on the city description element
     this.cityDescription.nativeElement.classList.toggle('cutoff-text');
     this.cityDescription.nativeElement.classList.toggle('expanded');
   }
